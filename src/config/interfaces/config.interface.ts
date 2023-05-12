087eb2594978b6db522b0aacfb57313c50655633
soldey <kmsoldatov@gmail.com>
2023-05-13 01:14:38 +0200

    feat(): initial v2

---
+++ src/config/interfaces/config.interface.ts
@@ -0,0 +1,8 @@
+/**
+ * Type of application operating modes, defined as: production, development
+ * @example production
+ */
+export enum ConfigMode {
+  'production' = 'production',
+  'development' = 'development',
+}
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+import { ConfigService } from './config';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const validationPipe = new ValidationPipe({
+    forbidNonWhitelisted: true,
+    forbidUnknownValues: true,
+    whitelist: true,
+    transform: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  });
+
+  app.setGlobalPrefix(configService.get('PREFIX'));
+
+  return app
+    .useGlobalPipes(validationPipe)
+    .listen(configService.get('PORT'), configService.get('HOST'));
+}
+bootstrap();
