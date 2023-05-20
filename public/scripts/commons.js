1007fd89dadf61cab56872ed73f07dbaf0465c5f
soldey <kmsoldatov@gmail.com>
2023-05-20 15:10:56 +0200

    chore: url

--- public/scripts/commons.js
+++ public/scripts/commons.js
@@ -3,5 +3,5 @@ const getCookie = ((name) => {
   var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
   if (match) return match[2];
 });
-const baseUrl = "http://localho.st:8000";
+const baseUrl = "https://soldey.onrender.com";
 const baseAPIurl = baseUrl + "/api/v1/";
